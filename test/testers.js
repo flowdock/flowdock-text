6e2a1f223381c1fa4aea1ac82ea5c5365ed82a1c
Ville Ikonen <ville.ikonen@nodeta.fi>
2012-02-10 10:08:12 +0200

    remove unused testers

--- test/testers.js
+++ test/testers.js
@@ -15,10 +15,6 @@ var testers = {
           return function(test) {
           return FlowdockText.autoLinkMentions(test.text);
         };
-        case "lists":
-          return function(test) {
-          return FlowdockText.autoLinkUsernamesOrLists(test.text);
-        };
         case "all":
           return function(test) {
           return FlowdockText.autoLink(test.text);
@@ -42,21 +38,6 @@ var testers = {
           return function(test) {
           return FlowdockText.extractMentionsWithIndices(test.text, ["@username", "@everyone", "@everybody", "@all", "@anyone", "@anybody"]);
         };
-        case "mentions_or_lists_with_indices":
-          return function(test) {
-          var results = FlowdockText.extractMentionsOrListsWithIndices(test.text);
-          return results.map(function(res) {
-            res['screen_name'] = res.screenName;
-            res['list_slug'] = res.listSlug;
-            delete res.screenName;
-            delete res.listSlug;
-            return res;
-          });
-        };
-        case "replies":
-          return function(test) {
-          return FlowdockText.extractReplies(test.text);
-        };
         case "urls":
           return function(test) {
           return FlowdockText.extractUrls(test.text);
@@ -74,28 +55,8 @@ var testers = {
           return FlowdockText.extractHashtagsWithIndices(test.text);
         };
       }
-      case "hit_highlighting":
-        return function(test) {
-        return FlowdockText.hitHighlight(test.text, test.hits);
-      };
       case "validate":
         switch (section) {
-        case "tweets":
-          return function(test) {
-          return FlowdockText.isValidTweetText(test.text);
-        };
-        case "usernames":
-          return function(test) {
-          return FlowdockText.isValidUsername(test.text);
-        };
-        case "lists":
-          return function(test) {
-          return FlowdockText.isValidList(test.text);
-        };
-        case "hashtags":
-          return function(test) {
-          return FlowdockText.isValidHashtag(test.text);
-        };
         case "urls":
           return function(test) {
           return FlowdockText.isValidUrl(test.text);
