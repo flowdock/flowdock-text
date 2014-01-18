bc2ddcb242f1ad5ecb6d31ef980411bdb649797f
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-01-18 18:10:54 +0200

    Fairer benchmark

--- benchmark.js
+++ benchmark.js
@@ -26,29 +26,17 @@
   .add("All", function() {
     var i;
     for (var i = samples.length; i >= 0; i--) {
-      FlowdockText.extractUrls(samples[i]);
-      FlowdockText.extractHashtags(samples[i]);
-      FlowdockText.extractMentions(samples[i]);
-      FlowdockText.extractEmails(samples[i]);
+      FlowdockText.extractAll(samples[i]);
     }
   })
   .add("Long: 1000 evil chars", function() {
-    FlowdockText.extractUrls(samples[0]);
-    FlowdockText.extractHashtags(samples[0]);
-    FlowdockText.extractMentions(samples[0]);
-    FlowdockText.extractEmails(samples[0]);
+    FlowdockText.extractAll(samples[0]);
   })
   .add("Short: 100 evil chars", function() {
-    FlowdockText.extractUrls(samples[900]);
-    FlowdockText.extractHashtags(samples[900]);
-    FlowdockText.extractMentions(samples[900]);
-    FlowdockText.extractEmails(samples[900]);
+    FlowdockText.extractAll(samples[900]);
   })
   .add("Nice: 0 evil chars", function() {
-    FlowdockText.extractUrls(samples[999]);
-    FlowdockText.extractHashtags(samples[999]);
-    FlowdockText.extractMentions(samples[999]);
-    FlowdockText.extractEmails(samples[999]);
+    FlowdockText.extractAll(samples[999]);
   })
   .add("parseTags: all", function() {
     var i;
